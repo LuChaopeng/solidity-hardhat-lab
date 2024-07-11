c15acee07c5a932cedf8c8a02c40259ccce9b84a
LuChaopeng <chaopeng.lu@qq.com>
2024-07-11 22:55:10 +0800

    test: 使用fixture

--- hardhat-start/test/my-tests.ts
+++ hardhat-start/test/my-tests.ts
@@ -1,37 +1,42 @@
 import { expect } from 'chai';
 import hre from 'hardhat';
-import { time } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { loadFixture, time } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
 describe("Lock", async () => {
+  /** beforeEach
+  let lock: any;
+  let unlockTime: number;
+  let lockedAmount = 100000000;
+  beforeEach(async () => {
+    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+    unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+    lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+      value: lockedAmount,
+    });
+  });
+  */
 
-  it("需要设置正确的解锁时间", async () => {
+  const deployOneYearLockFixture = async () => {
     const lockedAmount = 1_000_000_000;
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const currentTime = await time.latest();
-    const unlockTime = currentTime + ONE_YEAR_IN_SECS;
+    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+
     const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
       value: lockedAmount,
     });
+    return {lock, unlockTime, lockedAmount};
+  }
+
+  it("需要设置正确的解锁时间", async () => {
+    const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
     expect(await lock.unlockTime()).to.equal(unlockTime);
   });
   it("过早提币报错", async () => {
-    const lockedAmount = 1_000_000_000;
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const currentTime = await time.latest();
-    const unlockTime = currentTime + ONE_YEAR_IN_SECS;
-    const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-      value: lockedAmount,
-    });
+    const { lock } = await loadFixture(deployOneYearLockFixture);
     await expect(lock.withdraw()).to.be.revertedWith("You can't withdraw yet");
   });
   it("到期成功提币", async () => {
-    const lockedAmount = 1_000_000_000;
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const currentTime = await time.latest();
-    const unlockTime = currentTime + ONE_YEAR_IN_SECS;
-    const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-      value: lockedAmount,
-    });
+    const { unlockTime, lock } = await loadFixture(deployOneYearLockFixture);
     await time.increaseTo(unlockTime);
     await lock.withdraw();
   });
