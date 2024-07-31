ed3f74672507feb18fa8b134d2d169f2b72b2342
LuChaopeng <chaopeng.lu@qq.com>
2024-07-31 17:57:08 +0800

    feat: 添加accounts的task

--- solidity102/hardhat.config.ts
+++ solidity102/hardhat.config.ts
@@ -1,6 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+task("accounts", "Prints account(s)")
+  .addOptionalParam("index", "Index of accounts array.")
+  .setAction(async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+    if (taskArgs.index === undefined) {
+      for (const account of accounts) {
+        console.log(account.address);
+      };
+      return;
+    }
+    const index = Number(taskArgs.index);
+    if (!Number.isInteger(index) || index < 0) {
+      console.log('Error: Invalid index parameter.');
+      return;
+    }
+    if (index >= accounts.length) {
+      console.log('Error: Index is greater than the number of accounts.');
+      return;
+    }
+    console.log(accounts[taskArgs.index].address);
+  });
+
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
 };
