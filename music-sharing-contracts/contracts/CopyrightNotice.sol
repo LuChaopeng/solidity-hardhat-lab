743b42ec264699dad0d5ab08cfa2a83d3dbbddf7
LuChaopeng <chaopeng.lu@qq.com>
2024-08-25 01:34:34 +0800

    feat: 版权公示合约

---
+++ music-sharing-contracts/contracts/CopyrightNotice.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract CopyrightNotice {
+  struct WorkMetaInfo {
+    string name;
+    string lyricist;
+    string composer;
+    uint releaseDate;
+  }
+
+  struct Copyright {
+    address copyrightHolder; // institution recover it from signature
+    bytes32 arweaveTxId;
+    bytes32 r;
+    bytes32 s;
+    uint8 v;
+    WorkMetaInfo metaInfo;
+  }
+
+  mapping (string => Copyright) copyrightList;
+  address administrator;
+
+  constructor() {
+    administrator = msg.sender;
+  }
+
+
+  function  addCopyright(bytes32 arwreaveTxId, bytes32 r, bytes32 s, uint8 v, WorkMetaInfo memory info, string memory uid)  external {
+    // check permission
+    require(msg.sender == administrator, 'NO PERMISSION');
+    // recover address
+    bytes32 txIdHash = keccak256(abi.encodePacked(arwreaveTxId));
+    address signer = ecrecover(txIdHash, v,r,s);
+    Copyright memory copyright = Copyright(signer, arwreaveTxId, r, s, v, info);
+    copyrightList[uid] = copyright;
+  }
+
+} 
